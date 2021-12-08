d3f52ef77d141b59ca6ac7ee5306cf11d7f52cfb
walkthunder Cooper <walkthunder@163.com>
2021-12-08 23:02:53 +0800

    feat(init): Init page.

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>游戏推荐<a href="https://www.dongjizupai.com/wp-admin">性感沙滩合集</a></h2>
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
